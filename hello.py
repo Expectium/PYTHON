9b547a625a49a705f98882f51db05d99de934c98
Expectium <silaatik0@gmail.com>
2022-12-01 23:15:58 +0300

    story 1

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print ("hello")
+character_name = "Mary"
+character_age = "42"
+print("There was a woman named " +  character_name + ", ")
+print("she was " + character_age + " years old.")
+character_name = "Alice"
+character_age= "22"
+print("She loved being " + character_age+ ",")
+print("but she didnt like her name")
